66123a57ac197410978b5c3192ca155d4af35520
mmmmmtasty <michael@mjejohnson.com>
2017-09-16 16:14:40 +0000

    2017-09-16: Committing latest configurations

--- commit_things.sh
+++ commit_things.sh
@@ -3,8 +3,8 @@
 # Take a copy of the appdaemon configuration and sed out the password
 sed -r 's/ha_key.+$/ha_key: IllNeverTell/gm' /home/pi/appdaemon/conf/appdaemon.yaml > /home/homeassistant/.homeassistant/appdaemon-apps/appdaemon.yaml
 cp /home/pi/appdaemon/conf/apps.yaml /home/homeassistant/.homeassistant/appdaemon-apps/apps.yaml
-cp /home/pi/appdaemon/conf/custom_css/* /home/homeassistant/.homeassistant/appdaemon-apps/custom_css/
-cp /home/pi/appdaemon/conf/custom_widgets/* /home/homeassistant/.homeassistant/appdaemon-apps/custom_widgets/
+cp -r /home/pi/appdaemon/conf/custom_css/* /home/homeassistant/.homeassistant/appdaemon-apps/custom_css/
+cp -r /home/pi/appdaemon/conf/custom_widgets/* /home/homeassistant/.homeassistant/appdaemon-apps/custom_widgets/
 
 # Commit the latest version
 git add .
