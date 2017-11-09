057a9df164ca97d71743f3ec0e7f49802e0fe69d
mmmmmtasty <michael@mjejohnson.com>
2017-11-09 19:36:57 +0000

    2017-11-09: Committing latest configurations

--- appdaemon-apps/update_house_mode.py
+++ appdaemon-apps/update_house_mode.py
@@ -10,24 +10,29 @@ import datetime
 
 class UpdateHouseMode(appapi.AppDaemon):
   def initialize(self):
+    morning_start = self.parse_time(self.get_state(self.args['morning_start']))
+    day_start = self.parse_time(self.get_state(self.args['day_start']))
+    evening_start = self.parse_time(self.get_state(self.args['evening_start']))
+    night_start = self.parse_time(self.get_state(self.args['night_start']))
+
     # Work out which mode we should be in now and reset it
     current_mode = 'morning'
-    if self.time() > self.parse_time(self.args["day_start"]):
+    if self.time() > day_start:
       current_mode = 'day' 
-    if self.time() > self.parse_time(self.args["evening_start"]):
+    if self.time() > evening_start:
       current_mode = 'evening' 
-    if self.time() > self.parse_time(self.args["night_start"]):
+    if self.time() > night_start:
       current_mode = 'night'
     self.update_mode({'new_mode': current_mode}) 
     
     # Work out the times from the config file and register daily callbacks
-    self.run_daily(self.update_mode, self.parse_time(self.args["morning_start"]), new_mode = "morning")
-    self.run_daily(self.update_mode, self.parse_time(self.args["day_start"]), new_mode = "day")
-    self.run_daily(self.update_mode, self.parse_time(self.args["evening_start"]), new_mode = "evening")
-    self.run_daily(self.update_mode, self.parse_time(self.args["night_start"]), new_mode = "night")
+    self.run_daily(self.update_mode, morning_start, new_mode = 'morning')
+    self.run_daily(self.update_mode, day_start, new_mode = 'day')
+    self.run_daily(self.update_mode, evening_start, new_mode = 'evening')
+    self.run_daily(self.update_mode, night_start, new_mode = 'night')
 
   def update_mode(self, args):
     current_mode = self.get_state(self.args['house_mode'])
     if current_mode != args['new_mode']:
-      self.log("[HOUSE MODE CHANGE] {} {}->{} ".format(self.args['house_mode'], current_mode, args['new_mode']))
+      self.log('[HOUSE MODE CHANGE] {} {}->{} '.format(self.args['house_mode'], current_mode, args['new_mode']))
       self.select_option(self.args['house_mode'], args['new_mode']) 
